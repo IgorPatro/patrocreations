bd51ab165946d4dc4caa544cd9d159a37346d9e4
Igor Patro <i.patro@wp.pl>
2021-02-27 13:07:50 +0100

    trying is husky and lint-staged working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
-const IndexPage = () => <h1>Hello World!</h1>
+const IndexPage = () => (
+  <h1>
+    Hello World! (added some text just to tryout is husky & lint-staged working)
+  </h1>
+)
 
 export default IndexPage
