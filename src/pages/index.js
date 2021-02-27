ebe9b3c44ff919fcbe7b3076e023bce132ccbc85
Igor Patro <i.patro@wp.pl>
2021-02-27 13:13:07 +0100

    trying is husky and lint-staged working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-const IndexPage = () => (
-  <h1>Hello World! another change to test husky and lint-staged</h1>
-)
+const IndexPage = () => <h1>Hello World! another change to test husk</h1>
 
 export default IndexPage
