4fab0d751ee24bfd7b85d11e1f9e9bb952dafb77
Igor Patro <i.patro@wp.pl>
2021-03-25 12:58:07 +0100

    changed styling system to emotion

--- src/templates/HomeTemplate.js
+++ src/templates/HomeTemplate.js
@@ -1,14 +1,40 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { css } from "@emotion/react"
 import "./globalStyles.css"
 
+const wrapperStyles = css`
+  width: calc(100vw - 20px);
+  height: calc(100vh - 20px);
+  background-color: red;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+`
+
+const navigationStyles = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`
+
+const contactStyles = css`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`
 
 const HomeTemplate = ({ children }) => (
   <>
-    <div>
-      <div>navigation</div>
+    <div css={wrapperStyles}>
+      <div css={navigationStyles}>navigation</div>
       <div>{children}</div>
-      <div>contact</div>
+      <div css={contactStyles}>contact</div>
     </div>
   </>
 )
